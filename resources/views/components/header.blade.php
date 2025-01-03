104dca9675de96783489318df6dcf6612b62a044
firfir9611 <firlifirmansyahagustin@gmail.com>
2025-01-03 16:53:56 +0700

    V1.5.32

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -241,6 +241,8 @@
                   </div>
                 </div>
                 @endif
+              @endif
+              @if(Auth::check())
                 <form method="POST" action="{{ route('logout') }}" class="flex justify-center">
                     @csrf
                     <button type="submit" class="flex gap-2 -mx-3 block w-full rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-100">
